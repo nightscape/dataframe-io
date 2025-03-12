7a595032309d3be83f5f4fe204882b0b84b7f1e3
Martin Mauch <martin@mauch.dev>
2025-03-12 12:15:19 +0100

    test: Add ETL integration test

---
+++ etl/src/test/scala/dev/mauch/spark/dfio/DockerLayer.scala
@@ -0,0 +1,26 @@
+package dev.mauch.spark.dfio
+// scalastyle:off
+import com.dimafeng.testcontainers.{DockerComposeContainer, ExposedService}
+import org.testcontainers.images.builder.ImageFromDockerfile
+import zio.testcontainers._
+import zio.{Task, ZIO, ZLayer, Scope}
+
+import java.time.Duration
+import java.util.Properties
+import java.io.File
+import java.nio.file.{Files, Paths}
+import java.io.PrintWriter
+import java.io.FileWriter
+
+import com.dimafeng.testcontainers.WaitingForService
+import org.testcontainers.containers.wait.strategy.WaitStrategy
+import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy
+import org.testcontainers.containers.output.OutputFrame
+import com.dimafeng.testcontainers.ServiceLogConsumer
+import com.dimafeng.testcontainers.KafkaContainer
+
+object DockerLayer {
+
+  val kafkaTestContainerLayer: ZLayer[Scope, Throwable, KafkaContainer] =
+    ZLayer.fromTestContainer(KafkaContainer())
+}
