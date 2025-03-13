890c4f78a3c5b0c63b1f33b8752f171c1e41fd6a
Martin Mauch <martin@mauch.dev>
2025-03-13 15:47:51 +0100

    feat: Structured Streaming support

--- etl/src/test/scala/dev/mauch/spark/dfio/ETLTest.scala
+++ etl/src/test/scala/dev/mauch/spark/dfio/ETLTest.scala
@@ -16,9 +16,20 @@ import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.spark.sql.{DataFrame, Encoder, Row, SparkSession}
 import java.io.File
-import java.nio.file.{Files, Paths}
-
+import java.nio.file.{Files, Path, Paths}
+import java.net.URLEncoder
 object ETLTest extends ZIOSpecDefault {
+  val sparkLayer: ZLayer[Scope, Throwable, SparkSession] = ZLayer.scoped(ZIO.acquireRelease {
+    ZIO.attempt {
+      SparkSession
+        .builder()
+        .appName("ETLTest")
+        .master("local[*]")
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+        .getOrCreate()
+    }.debug
+  }(spark => ZIO.succeed(spark.close())))
 
   case class Person(id: Long, name: String, age: Int)
   val exampleData: List[Person] = List(
@@ -28,12 +39,21 @@ object ETLTest extends ZIOSpecDefault {
     Person(4, "Dave", 40),
     Person(5, "Eve", 22)
   )
+  case class BossRelation(employeeId: Long, bossId: Option[Long])
+  val bossRelations: List[BossRelation] = exampleData.map(person => BossRelation(person.id, exampleData.filter(_.id > person.id).sortBy(_.id).headOption.map(_.id)))
+  case class Employee(id: Long, name: String, bossId: Option[Long])
+  val employees: List[Employee] = exampleData.map(person => Employee(person.id, person.name, bossRelations.find(_.employeeId == person.id).flatMap(_.bossId)))
+  val tempDirLayer: ZLayer[Scope, Throwable, Path] = ZLayer {
+    ZIO.acquireRelease(ZIO.attempt(Files.createTempDirectory("dataframe-io-test")))(dir =>
+      ZIO.attempt(deleteRecursively(dir)).ignoreLogged
+    )
+  }
 
-  private val testTopic = "test-topic"
-  private val testDeltaPath = {
-    val tempDir = Files.createTempDirectory("dataframe-io-test").toFile()
-    tempDir.deleteOnExit()
-    tempDir.getAbsolutePath()
+  private def deleteRecursively(path: Path): Unit = {
+    if (Files.isDirectory(path)) {
+      Files.list(path).forEach(deleteRecursively)
+    }
+    Files.delete(path)
   }
 
   private def producerSettings(bootstrapServers: String): ProducerSettings =
@@ -50,29 +70,24 @@ object ETLTest extends ZIOSpecDefault {
       .withProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
       .withCloseTimeout(5.seconds)
 
-  private def createSparkSession(): SparkSession = {
-    SparkSession.builder()
-      .appName("ETLTest")
-      .master("local[*]")
-      .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-      .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-      .getOrCreate()
-  }
-
   def spec = suite("ETL Test")(
     test("should run ETL from Kafka to Delta") {
       for {
+        spark <- ZIO.service[SparkSession]
+        testDeltaPath <- ZIO.service[Path]
+        randomNumber <- Random.nextIntBounded(1000)
+        topic <- ZIO.succeed(s"test-topic-$randomNumber")
         kafka <- ZIO.service[KafkaContainer]
         bootstrapServers = kafka.bootstrapServers
         producer <- Producer.make(producerSettings(bootstrapServers))
         _ <- ZIO.foreach(exampleData) { person =>
-               val json = s"""{"id": ${person.id}, "name": "${person.name}", "age": ${person.age}}"""
-               producer.produce(new ProducerRecord(testTopic, "key1", json), Serde.string, Serde.string)
-             }
+          val json = s"""{"id": ${person.id}, "name": "${person.name}", "age": ${person.age}}"""
+          producer.produce(new ProducerRecord(topic, "key1", json), Serde.string, Serde.string)
+        }
         _ <- ZIO.attempt {
           val args = s"""
             --master local[*]
-            --source kafka://${bootstrapServers.replaceFirst("PLAINTEXT://", "")}/$testTopic?serde=json
+            --source kafka://${bootstrapServers.replaceFirst("PLAINTEXT://", "")}/$topic?serde=json
             --sink console://foo
             --sink delta://$testDeltaPath
           """.split("\\s+").filter(_.nonEmpty)
@@ -80,10 +95,8 @@ object ETLTest extends ZIOSpecDefault {
           ETL.main(args)
         }
         result <- ZIO.attempt {
-          val spark = createSparkSession()
-          val deltaDF = spark.read.format("delta").load(testDeltaPath)
+          val deltaDF = spark.read.format("delta").load(testDeltaPath.toString())
           val rows = deltaDF.collect()
-          spark.close()
           rows
         }
 
@@ -92,6 +105,78 @@ object ETLTest extends ZIOSpecDefault {
         assert(result.map(_.getAs[Long]("id")).toSet)(equalTo(exampleData.map(_.id).toSet)) &&
         assert(result.map(_.getAs[String]("name")).toSet)(equalTo(exampleData.map(_.name).toSet))
       }
-    } @@ TestAspect.timeout(120.seconds)
-  ).provideSomeLayerShared(DockerLayer.kafkaTestContainerLayer)
+    } @@ TestAspect.timeout(60.seconds),
+    test("should run streaming ETL from Kafka to Delta") {
+      val thisExampleData = exampleData.map(person => Person(person.id, person.name, person.age))
+      for {
+        spark <- ZIO.service[SparkSession]
+        testDeltaPath <- ZIO.service[Path]
+        randomNumber <- Random.nextIntBounded(1000).map(_ + 1000)
+        topic <- ZIO.succeed(s"test-topic-$randomNumber")
+        kafka <- ZIO.service[KafkaContainer]
+        bootstrapServers = kafka.bootstrapServers
+        producer <- Producer.make(producerSettings(bootstrapServers))
+        _ <- ZIO.attempt {
+          val bossRelationsDF = spark.createDataFrame(bossRelations)
+          bossRelationsDF.write.format("delta").mode("overwrite").save(s"$testDeltaPath/bossRelations")
+        }
+        sql = """
+          SELECT
+            people.id,
+            people.name,
+            people.age,
+            bossRelations.bossId as bossId
+          FROM
+            people
+          LEFT JOIN
+            bossRelations
+          ON
+            people.id = bossRelations.employeeId
+        """.trim
+        // Start the streaming ETL job concurrently in a fiber.
+        etlFiber <- ZIO.attempt {
+          val schema = org.apache.spark.sql.Encoders.product[Person].schema
+          val schemaURL = java.net.URLEncoder.encode(schema.json, "UTF-8")
+          val args = s"""
+            --master local[*]
+            --source people+kafka-stream://${bootstrapServers.replaceFirst("PLAINTEXT://", "")}/$topic?serde=json:$schemaURL&startingOffsets=earliest
+            --source bossRelations+delta://${testDeltaPath}/bossRelations
+            --transform people+employees+sql:///${URLEncoder.encode(sql.replaceAll("\\s+", " "), "UTF-8")}
+            --sink employees+delta://$testDeltaPath/employees?checkpointLocation=$testDeltaPath/checkpoint
+          """.split("\\s+").filter(_.nonEmpty)
+          println("Starting ETL.main with: " + args.mkString(" "))
+          ETL.main(args)
+        }.debug.fork
+
+        // Allow the ETL job to fully initialize.
+        _ <- ZIO.sleep(2.seconds)
+
+        // Produce records and, after each send, check the intermediate Delta table state.
+        assertions <- ZStream
+          .fromIterable(thisExampleData.zipWithIndex)
+          .mapZIO { case (person, idx) =>
+            val json = s"""{"id": ${person.id}, "name": "${person.name}", "age": ${person.age}}"""
+            val employee = employees.find(_.id == person.id).get
+            for {
+              _ <- producer.produce(new ProducerRecord(topic, "key1", json), Serde.string, Serde.string)
+              assertion <- ZIO
+                .attempt {
+                  import spark.implicits._
+                  spark.read.format("delta").load(s"$testDeltaPath/employees").as[Employee].collect().toSeq
+                }
+                .map(rows => assert(rows)(contains(employee)))
+                .filterOrFail(_.isSuccess)(new RuntimeException(s"Row $idx: $employee not found"))
+                .retry(Schedule.spaced(250.milliseconds) && Schedule.recurs(60))
+            } yield assertion
+          }
+          .runCollect
+        _ <- etlFiber.interrupt
+      } yield {
+        assertions.reduce(_ && _)
+      }
+    } @@ TestAspect.timeout(60.seconds) @@ TestAspect.withLiveClock
+  )
+    .provideSomeLayer[Scope with KafkaContainer with SparkSession](tempDirLayer)
+    .provideSomeLayerShared[Scope with KafkaContainer](sparkLayer)
+    .provideSomeLayerShared(DockerLayer.kafkaTestContainerLayer)
 }
