7275ae79925f4256f4a0ffc9ddf3a2d7cd6f77ab
Scala Steward <scala_steward@virtuslab.com>
2025-03-15 20:23:53 +0000

    Reformat with scalafmt 3.8.6
    Executed command: scalafmt --non-interactive

--- core/src/main/scala/dev/mauch/spark/dfio/ValuesSource.scala
+++ core/src/main/scala/dev/mauch/spark/dfio/ValuesSource.scala
@@ -41,16 +41,15 @@ case class ValuesSource(spark: SparkSession, header: Seq[String], values: Seq[Se
 
 class ValuesUriParser extends DataFrameUriParser {
   def schemes: Seq[String] = Seq("values")
-  override def apply(uri: java.net.URI): SparkSession => DataFrameSource with DataFrameSink = {
-    spark =>
-        new ValuesSource(
-          spark,
-          header = uri.queryParams.getOrElse("header", "").split(",").toSeq,
-          values = uri.queryParams
-            .getOrElse("values", "")
-            .split(";")
-            .map(_.split(",").toSeq)
-            .toSeq
-        )
-    }
+  override def apply(uri: java.net.URI): SparkSession => DataFrameSource with DataFrameSink = { spark =>
+    new ValuesSource(
+      spark,
+      header = uri.queryParams.getOrElse("header", "").split(",").toSeq,
+      values = uri.queryParams
+        .getOrElse("values", "")
+        .split(";")
+        .map(_.split(",").toSeq)
+        .toSeq
+    )
+  }
 }
