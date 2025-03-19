840bc86fb3c4040c285262926d49e360d42a3d3d
Martin Mauch <martin@mauch.dev>
2025-03-19 16:35:16 +0100

    feat: values source can specify types in header

--- core/src/main/scala/dev/mauch/spark/dfio/ValuesSource.scala
+++ core/src/main/scala/dev/mauch/spark/dfio/ValuesSource.scala
@@ -6,37 +6,31 @@ import org.apache.spark.sql.types._
 import UriHelpers._
 import java.net.URI
 
-case class ValuesSource(spark: SparkSession, header: Seq[String], values: Seq[Seq[String]])
+case class ValuesSource(spark: SparkSession, header: Seq[(String, DataType)], values: Seq[Seq[String]])
     extends DataFrameSource
     with DataFrameSink {
   def valuesToDF(f: Seq[String] => Row): DataFrame = {
     import spark.implicits._
     spark.createDataFrame(
       java.util.Arrays.asList(values.map(f): _*),
-      StructType(header.map(StructField(_, StringType)))
+      StructType(header.map { case (name, dt) => StructField(name, dt) })
     )
   }
   override def read(): DataFrame = {
-    import spark.implicits._
-    header match {
-      case Seq(a) => valuesToDF { case Seq(a) => Row(a) }
-      case Seq(a, b) =>
-        valuesToDF { case Seq(a, b) => Row(a, b) }
-      case Seq(a, b, c) =>
-        valuesToDF { case Seq(a, b, c) => Row(a, b, c) }
-      case Seq(a, b, c, d) =>
-        valuesToDF { case Seq(a, b, c, d) => Row(a, b, c, d) }
-      case Seq(a, b, c, d, e) =>
-        valuesToDF { case Seq(a, b, c, d, e) => Row(a, b, c, d, e) }
-      case Seq(a, b, c, d, e, f) =>
-        valuesToDF { case Seq(a, b, c, d, e, f) => Row(a, b, c, d, e, f) }
+    def convertValue(s: String, dt: DataType): Any = dt match {
+      case IntegerType => s.toInt
+      case DoubleType => s.toDouble
+      case LongType => s.toLong
+      case _ => s
+    }
+    valuesToDF { row =>
+      Row.fromSeq(header.zip(row).map { case ((_, dt), value) => convertValue(value, dt) })
     }
   }
   override def write(df: DataFrame): Boolean = {
     df.show(10000, false)
     true
   }
-
 }
 
 class ValuesUriParser extends DataFrameUriParser {
