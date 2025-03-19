948b765e9ad1ac0490091f413fe5615c78fa8264
Martin Mauch <martin@mauch.dev>
2025-03-19 14:50:52 +0100

    refactor: Make transformations runtime-discoverable

--- core/src/main/scala/dev/mauch/spark/dfio/TransformerParser.scala
+++ core/src/main/scala/dev/mauch/spark/dfio/TransformerParser.scala
@@ -3,24 +3,25 @@ package dev.mauch.spark.dfio
 import java.net.URLDecoder
 import org.apache.spark.sql.DataFrame
 
-trait TransformerParser {
-  def unapply(uri: java.net.URI): Option[DataFrame => DataFrame]
+trait TransformerParser extends PartialFunction[java.net.URI, DataFrame => DataFrame] {
+  def sparkConfigs: Map[String, String] = Map.empty
+  def schemes: Seq[String]
+  def isDefinedAt(uri: java.net.URI): Boolean = schemes.contains(uri.getScheme)
 }
-object TransformerParser extends TransformerParser {
-  def unapply(uri: java.net.URI): Option[DataFrame => DataFrame] = {
-    uri.getScheme match {
-      case "identity" => Some(identity[DataFrame] _)
-      case "sql" =>
-        Some { df =>
-          df.createOrReplaceTempView("input")
-          df.sqlContext.sql(URLDecoder.decode(uri.getPath.substring(1), "UTF-8"))
-        }
-      case "sql-file" =>
-        Some { df =>
-          df.createOrReplaceTempView("input")
-          df.sqlContext.sql(scala.io.Source.fromFile(uri.getPath.substring(1)).mkString)
-        }
-      case _ => None
+
+class IdentityTransformerParser extends TransformerParser {
+  override def schemes: Seq[String] = Seq("identity")
+  override def apply(uri: java.net.URI): DataFrame => DataFrame = identity[DataFrame]
+}
+
+class SqlTransformerParser extends TransformerParser {
+  override def schemes: Seq[String] = Seq("sql", "sql-file")
+  override def apply(uri: java.net.URI): DataFrame => DataFrame = { df =>
+    val sql = uri.getScheme match {
+      case "sql" => URLDecoder.decode(uri.getPath.substring(1), "UTF-8")
+      case "sql-file" => scala.io.Source.fromFile(uri.getPath.substring(1)).mkString
     }
+    df.createOrReplaceTempView("input")
+    df.sqlContext.sql(sql)
   }
 }
